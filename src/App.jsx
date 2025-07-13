c6cc80731965e340f682be93e7ef7cfe680a1095
israt Jahan <israt0jahan7@gmail.com>
2025-07-14 00:02:20 +0200

    initial react state and hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,70 @@
+import {useState} from "react";
+
+/** React state:
+ * React rendering process relies on state and props to decide when and how to re-render a component
+ * when a state changes, react automatically re-renders and reflect the new changes on the screen such as like/unlike
+ */
+
+/** React hooks:
+ * Hooks are special func in react that allows to let you tap into react features like;
+ * for managing state (useState)
+ * for handling side effects e.g. data fetching (useEffect)
+ * for sharing data across components (useContext)
+ * for optimizing callback functions (useCallback)
+ * and plenty more (useReducer, useMemo, useRef, useTransition)
+ */
+
+/** Card component includes:
+ * @param title
+ * @param rating
+ * @param isCool
+ * @param actors
+ * @returns {JSX.Element}
+ * @constructor
+ */
+
 // Functional arrow component
 const Card = ({ title, rating=0, isCool=false, actors=[] }) => {
+    // defining a state [variable_name, set_var_name]
+    const [count, setCount] = useState(0);
+    const [hasLiked, setHasLiked] = useState(false);
+
+    /**
+     * My Effect runs twice when the component mounts
+     * because react runs setup and cleanup one extra time before the actual setup.
+     * (remove StrictMode wrapper from main.jsx to avoid this).
+     * it's a stress test that verifies Effect's logic is implemented correctly.
+     */
+    useEffect(() => {
+        console.log(`${title} has been liked: ${hasLiked}`);
+    }, [hasLiked]);
+
+    // most common
+    useEffect(() => {
+        console.log("card rendered");
+    }, [])
+
     return (
         /**
          * If I change same thing here and in "card" class, these style will work
          * because inline styles have preferences over all the other CSS styles
          */
-        <div className="card" style={{border: '1px solid #4b5362'}}>
+        <div
+            className="card"
+            style={{border: '1px solid #4b5362'}}
+        >
             <h2>{title}</h2>
+            <button
+                onClick={() => {
+                    setHasLiked(!hasLiked);
+                    setCount((prevState) =>  prevState + 1);
+                }
+            }
+                type="button"
+            >
+                {/* conditional rendering; a simple case: render when count has value*/}
+                {hasLiked ? '❤️' : '🤍'}{count || null}
+            </button>
         </div>
     )
 }
