e55813d9a52e1a64a8f89a94b53fcd54040a7fe4
Israt Jahan <israt0jahan7@gmail.com>
2025-07-23 23:45:45 +0200

    title and rating added in movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,9 +7,13 @@ const MovieCard = ({ movie:
     return (
         <div className="movie-card">
             <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'} alt={title} />
-            <div>
+            <div className="mt-4">
                 <h3>{title}</h3>
-                <div>{original_language}</div>
+                <div className="content">
+                    <div className="rating">
+                        ⭐<p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
